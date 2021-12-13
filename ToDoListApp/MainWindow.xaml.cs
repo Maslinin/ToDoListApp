e8eb267798d15412315adc420820c8eb4c4534c7
Danil Rudin <ddanilr@yandex.ru>
2021-12-13 16:46:30 +0300

    repairing formatting

--- ToDoListApp/MainWindow.xaml.cs
+++ ToDoListApp/MainWindow.xaml.cs
@@ -1,62 +1,61 @@
-﻿//Основной код программы:
+﻿using ToDoListApp.IOService;
+using ToDoListApp.Model;
+
 namespace ToDoListApp
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Interaction Logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : System.Windows.Window
     {
-        //Список задач/создание коллекции
         private static System.ComponentModel.BindingList<ToDoModel> _ToDoDataList;
-        //Объект класса FileIOService
-        private static readonly FIleIOService _FileIOService = new FIleIOService();
+        private static readonly FileIOService _FileIOService = new FileIOService();
 
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        //Здесь будут хранится данные из заметок в файле
         private void Window_Loaded(object sender, System.Windows.RoutedEventArgs e)
         {
             try
             {
-                //Обрщаемся к жесткому диску и загружаем данные из файла в ToDoDataList
                 _ToDoDataList = _FileIOService.LoadData();
             }
-            //Выводим сообщение о конкретной ошибке на экран:
             catch(System.Exception ex)
             {
-                System.Windows.MessageBox.Show(ex.Message, "Ошибка в работе", System.Windows.MessageBoxButton.OK, System.Windows.MessageBoxImage.Error);
-                //Закрываем приложение
+                System.Windows.MessageBox.Show(ex.Message,
+                    ex.GetType().Name, 
+                    System.Windows.MessageBoxButton.OK, 
+                    System.Windows.MessageBoxImage.Error);
+
                 System.Environment.Exit(0);
             }
 
-            //ItemSourse задает коллекцию
-            //DgToDoList - название DataGrid
             DgToDoList.ItemsSource = _ToDoDataList;
-            //Когда что то обновляется в списке, вызывается событие, что бы данные на HDD обновились
+
             _ToDoDataList.ListChanged += ToDoDataList_ListChanged;
         }
 
-        //Обработчик событий, когда что-то изменяется в списке
+        //Event handler when something changes in the list
         private void ToDoDataList_ListChanged(object sender, System.ComponentModel.ListChangedEventArgs e)
         {
-            //События, на которые реагирует лист:
+            //Events to which the sheet responds:
             if (e.ListChangedType == System.ComponentModel.ListChangedType.ItemAdded || 
                 e.ListChangedType == System.ComponentModel.ListChangedType.ItemDeleted || 
                 e.ListChangedType == System.ComponentModel.ListChangedType.ItemChanged)
             {
                 try
                 {
-                    //Сейвим данные из ToDoDataList
                     _FileIOService.SaveData(sender);
                 }
-                //Выводим сообщение о конкретной ошибке на экран:
                 catch (System.Exception ex)
                 {
-                    System.Windows.MessageBox.Show(ex.Message, "Ошибка в работе", System.Windows.MessageBoxButton.OK, System.Windows.MessageBoxImage.Error);
-                    //Закрываем приложение
+                    System.Windows.MessageBox.Show(ex.Message, 
+                        ex.GetType().Name, 
+                        System.Windows.MessageBoxButton.OK, 
+                        System.Windows.MessageBoxImage.Error);
+
                     Close();
                 }
             }
---
+++ ToDoListApp/Model/ToDoModel.cs
@@ -0,0 +1,51 @@
+﻿using System.ComponentModel;
+
+namespace ToDoListApp.Model
+{
+    sealed class ToDoModel : INotifyPropertyChanged
+    {
+        //Open field that will set the time automatically
+        public string CreationDate { get; private set; } = System.DateTime.Now.ToShortDateString();
+        //Completed task or not
+        public bool _IsDone { get; private set; }
+        //Task Field:
+        public string _Text { get; private set; }
+
+        //Property for the Status column
+        public bool IsDone
+        {
+            get { return _IsDone; }
+            set
+            {
+                if (_IsDone == value) 
+                    return;
+
+                _IsDone = value;
+                OnPropertyChanged("IsDone");
+            }
+        }
+
+        //Property for a column with a task
+        public string Text
+        {
+            get { return _Text; }
+            set
+            {
+                if (_Text == value) 
+                    return;
+
+                _Text = value;
+                OnPropertyChanged("Text");
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        //Processing of event (change) in the PropertyChanged
+        private void OnPropertyChanged(string PropertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+    }
+}
